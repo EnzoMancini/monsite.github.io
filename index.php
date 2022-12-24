0eb396fdc14315a3d5d28b3a927704f57b152362
EnzoMancini <95383732+EnzoMancini@users.noreply.github.com>
2022-12-24 14:55:49 +0100

    ajout index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <link rel="shortcut icon" href="" />
+        <link rel="stylesheet" href="">
+        <title>Mon Site</title>
+    </head>
+    <body>
+        
+    </body>
+</html>
