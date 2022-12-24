827de7f229215c4ed6fd017bdec1c8d957f4b223
EnzoMancini <95383732+EnzoMancini@users.noreply.github.com>
2022-12-24 15:01:45 +0100

    test

--- index.php
+++ index.php
@@ -3,10 +3,10 @@
     <head>
         <meta charset="utf-8"/>
         <link rel="shortcut icon" href="" />
-        <link rel="stylesheet" href="">
+        <link rel="stylesheet" href="styles/style.css">
         <title>Mon Site</title>
     </head>
     <body>
-        
+        Enzo Mancini
     </body>
 </html>
